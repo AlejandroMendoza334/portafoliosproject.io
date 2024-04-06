6a58d3a4eb777ea9ddd172e62e5c383dcc968717
Alejandro Mendoza <89280136+AlejandroMendoza334@users.noreply.github.com>
2024-04-06 00:58:33 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+//Funcion para filtrar las categorias de los trabajos
+function verCategoria(cat){
+    const items = document.getElementsByClassName("item");
+    for(let i=0; i < items.length;i++){
+        items[i].style.display = "none";
+    }
+
+    const itemCat = document.getElementsByClassName(cat);
+    for(let i = 0; i<itemCat.length;i++){
+        itemCat[i].style.display = "block";
+    }
+
+    const links = document.querySelectorAll(".trabajos nav a");
+    links[0].className = "";
+    links[1].className = "";
+    links[2].className = "";
+    links[3].className = "";
+
+    const itemSeleccionado = document.getElementById(cat);
+    itemSeleccionado.className = "borde"
+}
+
+function responsiveMenu(){
+    let x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+
+        // creamos el elemento que cierra el menu
+        let span = document.createElement("span");
+        span.innerHTML = "CLOSE";
+        document.getElementById("nav").appendChild(span);
+
+        // quitamos el boton eliminar cuando se hace click sobre este
+        span.onclick = function(){
+                x.className = "";
+                span.remove();
+            }
+        }else{
+            x.className="";
+        }
+    }
+// este codigo es para agregar una funcion a cada links del menu
+// responsive
+// cuando se presione en cualquier de los links del menu debe desaparecer el menu
+const links = document.querySelectorAll("#nav a");
+for(i=0; i < links.length;i++){
+    links[i].onclick = function(){
+        var x = document.getElementById("nav");
+        x.className = "";
+
+        //removemos el boton
+        btnEliminar = document.querySelector("#nav span");
+        btnEliminar.remove();
+    }
+}
